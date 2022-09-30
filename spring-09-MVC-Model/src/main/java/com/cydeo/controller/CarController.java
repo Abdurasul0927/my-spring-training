4d70d51fcea7729cf198bf44455c4dc5be0cabb7
rasulshirinboev <rasulboev9@gmail.com>
2022-09-30 14:06:36 -0400

    commit

--- spring-09-MVC-Model/src/main/java/com/cydeo/controller/CarController.java
+++ spring-09-MVC-Model/src/main/java/com/cydeo/controller/CarController.java
@@ -2,6 +2,7 @@ package com.cydeo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -35,5 +36,14 @@ public class CarController {
         model.addAttribute("year",year);
 
         return "car/car-info";
+    }
+
+    @RequestMapping("/info/{make}/{year}") //localhost:8080/info/honda
+    public String getCArInfo(@PathVariable String make,@PathVariable int year){
+
+        System.out.println(make);
+        System.out.println(year);
+        return "car/car-info";
+
     }
 }
