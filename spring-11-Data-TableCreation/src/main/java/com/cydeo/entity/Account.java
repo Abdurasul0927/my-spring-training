062bc0471b93125444839af589fcc412a4e513fd
rasulshirinboev <rasulboev9@gmail.com>
2023-01-10 00:39:30 -0500

    Spring Data

--- spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Account.java
+++ spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Account.java
@@ -1,6 +1,8 @@
 package com.cydeo.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 import java.math.BigDecimal;
@@ -9,6 +11,7 @@ import java.math.BigDecimal;
 public class Account {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String owner;
     private BigDecimal balance;
