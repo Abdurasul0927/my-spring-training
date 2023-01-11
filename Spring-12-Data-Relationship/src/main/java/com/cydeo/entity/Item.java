a19d4117fbf129f956cec25ced49e96ec756216c
rasulshirinboev <rasulboev9@gmail.com>
2023-01-11 02:02:47 -0500

    Table Relationship

--- Spring-12-Data-Relationship/src/main/java/com/cydeo/entity/Item.java
+++ Spring-12-Data-Relationship/src/main/java/com/cydeo/entity/Item.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Entity
 @Data
 @NoArgsConstructor
@@ -16,4 +18,11 @@ public class Item {
 
     private String name;
     private String code;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Cart> carts;
+    public Item(String name, String code) {
+        this.name = name;
+        this.code = code;
+    }
 }
